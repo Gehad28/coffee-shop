b334de661c76ced17408eb8ea0c48135288380b2
جهاد أحمد <gehad0ahmed28@gmail.com>
2024-10-20 18:08:14 +0300

    Cart Saving

--- modules/pages/CheckoutPage.js
+++ modules/pages/CheckoutPage.js
@@ -27,7 +27,10 @@ export default class CheckoutPage extends HTMLElement {
     connectedCallback() {
         window.addEventListener("appcartchange", () => {
             this.render();
-        })
+        });
+        if (localStorage.getItem("cart")) {
+            app.store.cart = JSON.parse(localStorage.getItem("cart"));
+        }
         this.render();
     }
 
